3e238ad61a4bf2f4ad88d5650340253948ffff7c
BARIN FRANCESCO <12999355@itisrossi.edu.it>
2026-02-21 08:34:50 +0100

    Creazione pila

---
+++ Lista/Lista/Nodo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista
+{
+    public class Nodo<T>
+    {
+        public T Data { get; private set; }
+        public Nodo<T> Previous { get; set; }
+
+        public Nodo(T value) 
+        {
+            Data = value;
+            Previous = null;
+        }
+    }
+}
