10e022e248d94000590bdd6b94880093a30e77fc
BARIN FRANCESCO <12999355@itisrossi.edu.it>
2026-03-10 11:27:29 +0100

    fine classi

---
+++ BinaryTree/BinaryTree/BinaryNode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    public class BinaryNode<T>
+    {
+
+        public T Value { get; set; }
+
+        public BinaryNode<T> LeftChild { get; set; }
+        public BinaryNode<T> RigthChild { get; set; }
+        
+
+        public BinaryNode( T value )
+        {
+            Value = value;
+            LeftChild = null;
+            RigthChild = null;
+        }
+    }
+}
---
+++ BinaryTree/BinaryTree/MyBinaryTree.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    public class MyBinaryTree<T>
+    {
+
+        BinaryNode<T> Root { get; set; }
+
+        public MyBinaryTree()
+        {
+            Root = null;
+        }
+
+        public void Insert( T Value)
+        {
+            BinaryNode<T> temp = new BinaryNode<T>(Value);
+
+            if(Root == null) 
+            {
+                Root = temp;
+            }else
+            {
+
+                (BinaryNode<T> node, bool direction) Lastnode = Getlastnode(Root);
+
+                if (Lastnode.direction)
+                {
+                    Lastnode.node.LeftChild = temp;
+                } else
+                {
+                    Lastnode.node.RigthChild = temp;
+                }
+                
+            }
+
+        }
+
+        public (BinaryNode<T> node, bool direction) Getlastnode(BinaryNode<T> MovingNode) 
+        {
+             
+            if (MovingNode.LeftChild == null)
+            {
+                return (MovingNode, true);
+            }
+            else if (MovingNode.RigthChild == null)
+            {
+                return (MovingNode, false);
+            }
+            else //if (MovingNode.RigthChild == null && MovingNode.LeftChild == null)
+            {
+                return Getlastnode(MovingNode.LeftChild);
+
+            }
+        }
+
+        public void PrintTree()
+        {
+            PrintTree(Root);
+        }
+
+        private void PrintTree(BinaryNode<T> node)
+        {
+            if (node == null)
+                return;
+
+            Console.WriteLine(node.Value);
+
+            PrintTree(node.LeftChild);
+            PrintTree(node.RigthChild);
+        }
+        //funzioen per la stampa 
+        /*
+         * insert
+         * */
+    }
+}
